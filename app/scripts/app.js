c68264249854f56b6bb9b50c5281ef906adbe404
Francisco Martinez <franciscomartinez919@gmail.com>
2014-11-19 22:54:14 -0600

    Initial commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name somewherebetweenusMobileWebApp
+ * @description
+ * # somewherebetweenusMobileWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('somewherebetweenusMobileWebApp', []);
