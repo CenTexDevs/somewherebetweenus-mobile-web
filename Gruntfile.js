7857b2b0a48c5e9f1a3f2e22f4857a7373c399d6
Rob Wooten <9147757@gmail.com>
2014-12-26 12:42:22 -0600

    adding jquery.cookie

--- Gruntfile.js
+++ Gruntfile.js
@@ -351,6 +351,11 @@ module.exports = function (grunt) {
         }, {
           expand: true,
           cwd: '.',
+          src: 'bower_components/jquery.cookie/*',
+          dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: '.',
           src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
           dest: '<%= yeoman.dist %>'
         }]
