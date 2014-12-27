66da8b028f353f9d772f64f3629ead0fe7d98e6d
Rob Wooten <9147757@gmail.com>
2014-12-27 11:14:26 -0600

    adding menu icon

--- Gruntfile.js
+++ Gruntfile.js
@@ -341,7 +341,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'fonts/{,*/}*.*'
+            'fonts/{,*/}*.*',
+            'styles/font-awesome.css'
           ]
         }, {
           expand: true,
