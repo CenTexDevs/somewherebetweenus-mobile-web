c8cf809c04eaef7525bb130bc5980720510a9e6d
Rob Wooten <9147757@gmail.com>
2014-12-31 16:11:37 -0600

    working on invite/join functionality

--- web.js
+++ web.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+
+app.post('/joingroup', function(req, res) {
+    res.write('in joingroup handler');
+	res.end();
+});
+
 app.use(express.static(__dirname + '/dist'));
 app.listen(port);
 console.log('Listening on port 3000');
