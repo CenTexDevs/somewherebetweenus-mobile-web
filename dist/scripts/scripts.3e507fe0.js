a4ac7558a9690125bc357af6ceffab3071da8039
Rob Wooten <9147757@gmail.com>
2015-01-29 11:45:32 -0600

    cleanup

---
+++ dist/scripts/scripts.3e507fe0.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("somewherebetweenusMobileWebApp",[]),angular.module("somewherebetweenusMobileWebApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
