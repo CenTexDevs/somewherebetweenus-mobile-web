f23d8d0a08be6f745bd2807a9e69c6926e08fef5
Rob Wooten <9147757@gmail.com>
2015-01-29 11:20:39 -0600

    troubleshooting git

--- dist/scripts/scripts.3e507fe0.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("somewherebetweenusMobileWebApp",[]),angular.module("somewherebetweenusMobileWebApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
